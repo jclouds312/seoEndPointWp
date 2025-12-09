0a4b931b27f22b6a63d11a7d27f1380d5401fced
universalbussinestechnicals <universalbussinestechnicals@gmail.com>
2025-12-09 21:57:36 +0000

    ok

--- server/index.ts
+++ server/index.ts
@@ -75,7 +75,9 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (process.env.NODE_ENV === "production") {
-    serveStatic(app);
+    if (!process.env.VERCEL) {
+      serveStatic(app);
+    }
   } else {
     const { setupVite } = await import("./vite");
     await setupVite(httpServer, app);
