1677e95291c7d737a147ad439036295648a3bfeb
universalbussinestechnicals <universalbussinestechnicals@gmail.com>
2025-12-09 21:48:22 +0000

    ok

--- server/static.ts
+++ server/static.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(process.env.STATIC_DIR || path.join(__dirname, "public"));
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
